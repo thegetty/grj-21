74fb0f72ae965b287dacf592e279543d557a8e58
Greg Albers <galbers@getty.edu>
2025-12-09 16:29:11 -0800

    Update for Getty's google tag manager implementation of GA4

--- _includes/components/analytics.js
+++ _includes/components/analytics.js
@@ -1,3 +1,7 @@
+//
+// CUSTOMIZED FILE
+// Update for Getty's google tag manager implementation of GA4
+//
 import { html } from '#lib/common-tags/index.js'
 
 /**
@@ -10,13 +14,7 @@ export default function (eleventyConfig) {
   return function (params) {
     if (!googleId) return ''
     return html`
-      <script async src="https://www.googletagmanager.com/gtag/js?id=${googleId}"></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${googleId}');
-      </script>
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${googleId}');</script>
     `
   }
 }
