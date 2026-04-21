78fd76d273a68326dc8c7261826fc1fbb1614648
Greg Albers <galbers@getty.edu>
2026-04-21 13:31:39 -0700

    Add proper URL line breaks

--- _plugins/markdown/index.js
+++ _plugins/markdown/index.js
@@ -68,14 +68,32 @@ export default function (eleventyConfig, options) {
       return self.renderToken(tokens, idx, options)
     }
 
-  /**
-   * Render external links so that they open in a new tab
-   */
   markdownLibrary.renderer.rules.link_open = (tokens, idx, options, env, self) => {
+    /**
+     * Render external links so that they open in a new tab
+     */
     const href = tokens[idx].attrGet('href')
     if (href.startsWith('http')) {
       tokens[idx].attrSet('target', '_blank')
     }
+
+    /**
+     * Insert zero-width space with punctuation for better line breaks in URLs
+     * per Chicago Manual of Style
+     */
+    const linkTextIndex = idx + 1
+    const breakAfter = /([[\/]{2}|:])/g // double-slash and colon
+    const breakBefore = /([(?<!\/)\/(?!\/)|~|\.|,|_|?|#|%|=|+|&|-])/g // single-slash and others
+    const breakCharacter = '​' // zero-width space  
+
+    const linkText = tokens[linkTextIndex].content.includes('http') 
+      ? tokens[linkTextIndex].content
+        .replace(breakAfter, '$1' + breakCharacter)
+        .replace(breakBefore, breakCharacter + '$1')
+      : tokens[linkTextIndex].content
+    
+    tokens[linkTextIndex].content = linkText
+
     return defaultRender(tokens, idx, options, env, self)
   }
 
